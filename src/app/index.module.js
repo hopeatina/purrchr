a9931be6fb089532e0994303e73755d75b7da442
hopeatina <hopeatina@gmail.com>
2016-02-05 13:23:58 -0600

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gitHub', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
