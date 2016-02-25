9dce9183c4751708f81bdaaab3c7068b5c6754ce
hopeatina <hopeatina@gmail.com>
2016-02-24 20:05:08 -0600

    redirect to feed

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -122,7 +122,7 @@ exports.oauthCallback = function (strategy) {
           return res.redirect('/authentication/signin');
         }
 
-        return res.redirect(redirectURL || sessionRedirectURL || '/');
+        return res.redirect( sessionRedirectURL || '/');
       });
     })(req, res, next);
   };
