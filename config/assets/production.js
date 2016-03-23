2a45bdf986a3ee069e96c2dde2151acc1617f149
hopeatina <hopeatina@gmail.com>
2016-03-22 22:17:40 -0500

    add material design icons to production

--- config/assets/production.js
+++ config/assets/production.js
@@ -6,6 +6,7 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'https://fonts.googleapis.com/icon?family=Material+Icons'
       ],
       js: [
         'public/lib/angular/angular.min.js',
